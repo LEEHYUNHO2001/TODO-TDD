11558d36902d680387a63ce6dc8c07d87928b78d
이현호 <dlgusgh2001@naver.com>
2022-06-07 18:57:30 +0900

    :sparkles: input test

--- src/tests/TodoForm.test.tsx
+++ src/tests/TodoForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import { TodoForm } from '@/components/TodoForm';
 
@@ -9,5 +9,18 @@ describe('<TodoForm />', () => {
     const button = screen.getByText('등록하기');
     expect(input).toBeInTheDocument();
     expect(button).toBeInTheDocument();
+  });
+
+  it('changes input', () => {
+    render(<TodoForm />);
+    const input = screen.getByPlaceholderText('할 일을 입력하세요');
+    fireEvent.change(input, {
+      target: {
+        value: 'TDD-TODO',
+      },
+    });
+
+    expect(input).toHaveAttribute('value', 'TDD-TODO');
+    // expect(input.value).toBe('TDD-TODO');
   });
 });
