bb40e3e3408985a8ca8c1bcd6660d7dcd6625e1d
이현호 <dlgusgh2001@naver.com>
2022-06-07 18:57:47 +0900

    :white_check_mark: input test

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
-export const TodoForm = () => (
-  <form>
-    <input placeholder="할 일을 입력하세요" />
-    <button type="submit">등록하기</button>
-  </form>
-);
+export const TodoForm = () => {
+  const [value, setValue] = useState('');
+
+  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <form>
+      <input placeholder="할 일을 입력하세요" onChange={handleInput} value={value} />
+      <button type="submit">등록하기</button>
+    </form>
+  );
+};
