79eacfed2a32c55efecdb6a0b41b4fcb33b4e3c5
이현호 <dlgusgh2001@naver.com>
2022-06-07 15:30:22 +0900

    :recycle: test code
    remove 스냅샷

--- src/tests/TodoForm.test.tsx
+++ src/tests/TodoForm.test.tsx
@@ -4,11 +4,10 @@ import { TodoForm } from '@/components/TodoForm';
 
 describe('<TodoForm />', () => {
   it('has input and a button', () => {
-    const { container } = render(<TodoForm />);
+    render(<TodoForm />);
     const input = screen.getByPlaceholderText('할 일을 입력하세요');
-    const text = screen.getByText('등록하기');
+    const button = screen.getByText('등록하기');
     expect(input).toBeInTheDocument();
-    expect(text).toBeInTheDocument();
-    expect(container).toMatchSnapshot();
+    expect(button).toBeInTheDocument();
   });
 });
